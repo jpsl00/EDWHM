daebd193366d6a11613820a19185a5e596ac635b
jpsl00 <joaopedroluciano@gmail.com>
2017-06-29 22:30:42 -0300

    Summary goes here
    Too sleepy to write what changed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const app = express();
+var port = '8080'
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -16,16 +17,16 @@ app.post('/webhook', (req, res) => {
         method: 'POST',
         url: req.body.hook,
         json: {
-            'content':req.body.content,
-            'username':req.body.username,
-            'avatar_url':req.body.avatar_url,
-            'tts':req.body.tts
+            'content': req.body.content,
+            'username': req.body.username,
+            'avatar_url': req.body.avatar_url,
+            'tts': req.body.tts
         },
     });
     console.log(chalk.blue("[POST] ") + chalk.yellow(JSON.stringify(req.body)));
     res.sendStatus(200).end();
 });
 
-app.listen(80, () => {
-    console.log(chalk.green.bold("Server started!"));
+app.listen(port, () => {
+    console.log(chalk.green.bold("Server started on port "+port+"!"));
 });
--- public/saveLoad.js
+++ public/saveLoad.js
@@ -1,26 +1,26 @@
-// A shitty saving solution, a really shitty one
-
 function saveJSON() {
 	var json = {
-		'content':document.getElementById("msg").value,
-		'username':document.getElementById("usr").value,
-		'avatar_url':document.getElementById("avt").value,
-		'tts':document.getElementById("tts").value,
-		'hook':document.getElementById("hook").value,
+		'content': document.getElementById("msg").value,
+		'username': document.getElementById("usr").value,
+		'avatar_url': document.getElementById("avt").value,
+		'tts': document.getElementById("tts").value,
+		'hook': document.getElementById("hook").value,
 	};
 	return JSON.stringify(json)
 }
 
 function loadJSON() {
 	var json = JSON.parse(prompt());
-	if (json === null) {return}
+	if (json === null) {
+		return
+	}
 	document.getElementById('msg').value = json.content;
 	document.getElementById('usr').value = json.username;
 	document.getElementById('avt').value = json.avatar_url;
 	document.getElementById('tts').value = json.tts;
-	if(json.tts == "true"){
+	if (json.tts == "true") {
 		document.getElementById('tts').checked = 1;
-	}else if(json.tts == "false"){
+	} else if (json.tts == "false") {
 		document.getElementById('tts').checked = 0;
 	};
 	document.getElementById('hook').value = json.hook;
